316d441ed9397fc92ca44455669d130e455ba0e6
Maksim Demidov <maaksaa@gmail.com>
2025-10-01 19:52:44 +0200

    feat(quotes): add quote card component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import QuoteCard from '@/components/QuoteCard.vue'
+</script>
 
 <template>
   <h1>Daily Mind</h1>
+  <QuoteCard text="some text" />
 </template>
 
 <style scoped></style>
---
+++ src/components/QuoteCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const { text } = defineProps<{ text: string }>()
+</script>
+
+<template>
+  <div class="quote-card">
+    <div class="quote-card__text">
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.quote-card {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 600px;
+  height: 200px;
+  border-radius: 10px;
+  border: dashed 1px darkolivegreen;
+}
+</style>
